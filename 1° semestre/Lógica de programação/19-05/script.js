4fbd93ac179d57dc64de5aa82151cbafbebfbabc
João <jgablopes2008@gmail.com>
2025-05-19 08:40:35 -0300

    exercicio matriz

---
+++ 1° semestre/Lógica de programação/19-05/script.js
@@ -0,0 +1,12 @@
+let matriz = [
+    ['Beterraba', 'Abacaxi', 'Tesoura', 'Lua', 'Banana'],
+    ['Javali', 'Macaco', 'Cachorro', 'Gato', 'Cor'],
+    ['Alicate', 'Maionese', 'Maçã', 'Maionese', 'Joaquim'],
+    ['Ivonete', 'Louça', 'Balão', 'Tangerina', 'Júpiter'],
+    ['Beterraba', 'Tião', 'Carlos', '', 'Beterraba']
+]
+
+for (i = 0; i < matriz.length; i++){
+    console.log(matriz[i])
+
+}
