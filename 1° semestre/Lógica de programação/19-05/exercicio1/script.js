2d83d13d5373d09ad02c3366bc129dda9b51446e
João <jgablopes2008@gmail.com>
2025-05-19 09:41:21 -0300

    mais detalhes

--- 1° semestre/Lógica de programação/19-05/exercicio1/script.js
+++ 1° semestre/Lógica de programação/19-05/exercicio1/script.js
@@ -6,6 +6,14 @@ let matriz = [
     ['Beterraba', 'Tião', 'Carlos', '', 'Beterraba']
 ]
 
+matriz[0][4] = 'Dinossauro'
+matriz[1][1] = 'Goku'
+matriz[4][1] = ''
+
+matriz.splice(3, 1)
+
+matriz.push(['', 'Carla', '', 'Jéssica', ''])
+
 for (i = 0; i < matriz.length; i++){
     console.log(matriz[i])
 
