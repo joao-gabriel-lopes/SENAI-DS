2f0141ee3e8e195279686d9e5ad053ebcee8811f
João <jgablopes2008@gmail.com>
2025-05-29 13:53:06 -0300

    fru fru

--- 1° semestre/HTML/29-05/site_correio/app/pagina_principal/index.js
+++ 1° semestre/HTML/29-05/site_correio/app/pagina_principal/index.js
@@ -1,5 +1,5 @@
 function Menu(){
-    if (document.getElementById('menu').style.visibility == 'hidden'){
+    if (document.getElementById('menu').style.visibility != 'visible'){
         document.getElementById('menu').style.visibility = 'visible'
     } else {
         document.getElementById('menu').style.visibility = 'hidden' 
