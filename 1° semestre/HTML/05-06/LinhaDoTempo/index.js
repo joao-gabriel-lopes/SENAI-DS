4a253935094ea6fcac899938842d798719446dad
João <jgablopes2008@gmail.com>
2025-06-05 10:16:17 -0300

    Projeto linha do tempo

---
+++ 1° semestre/HTML/05-06/LinhaDoTempo/index.js
@@ -0,0 +1,11 @@
+function AbrirMenu(){
+
+    if (document.getElementById('menu').style.visibility != 'visible'){
+        document.getElementById('menu').style.visibility = 'visible'
+    
+    } else{
+        document.getElementById('menu').style.visibility = 'hidden'
+    
+    }
+    
+}
