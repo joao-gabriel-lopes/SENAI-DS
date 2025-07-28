70bd5a8b3076accfbd80bb6f27eb904b9a5368c3
João <jgablopes2008@gmail.com>
2025-07-28 14:54:22 -0300

    Arquivos de tela login com php

---
+++ 2° semestre/Programação Back-End/slides/28-07/autenticação/index.js
@@ -0,0 +1,8 @@
+function Autenticar(usuario, senha){
+    if (usuario == 'aluno' && senha == '1234') {
+        alert('BEM VINDO');        
+    }
+    else {
+        alert('USUÁRIO OU SENHA INVÁLIDOS')
+    }
+}
