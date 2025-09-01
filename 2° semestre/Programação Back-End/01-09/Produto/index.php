4400818fd1c94c9181c97a38450455fb42260f4c
João <jgablopes2008@gmail.com>
2025-09-01 13:12:14 -0300

    Exercício classe produto

---
+++ 2° semestre/Programação Back-End/01-09/Produto/index.php
@@ -0,0 +1,35 @@
+<?php
+    require_once 'produto.php';
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Produto</title>
+</head>
+
+<body>
+
+    <h1>Produto</h1>
+
+    <?php 
+        $produto = new Produto(
+            nome: "Leite",
+            valorUnitario: 6.95,
+            dataVencimento: new DateTime("30-10-2025")
+        );
+
+        echo "Nome do produto: " . $produto->GetNome() . "<br>";
+        echo "Valor unitário: " . $produto->GetValorUnitario() . "<br>";
+        echo "Data de Validade: " . $produto->GetDataVencimento() . "<br>";
+        echo "O produto está: ";
+        echo $produto->VerificarSeEstaVencido();
+    
+    ?>
+
+</body>
+
+</html>
---
+++ 2° semestre/Programação Back-End/01-09/Produto/produto.php
@@ -0,0 +1,66 @@
+<?php
+
+class Produto
+{
+    private $nome;
+    private $valorUnitario;
+    private $dataVencimento;
+
+    public function __construct($nome, $valorUnitario, $dataVencimento)
+    {
+        $this->SetNome($nome);
+        $this->setValorUnitario($valorUnitario);
+        $this->setDataVencimento($dataVencimento);
+    }
+
+    public function SetNome($parametroNome)
+    {
+        if (strlen($parametroNome) > 0) {
+            $this->nome = $parametroNome;
+        } else {
+            throw new InvalidArgumentException("O nome do produto não deve pode possuir zero ou menos caracteres");
+        }
+    }
+
+    public function SetValorUnitario($parametroValorUnitario): void
+    {
+        if ($parametroValorUnitario > 0) {
+            $this->valorUnitario = $parametroValorUnitario;
+        } else {
+            throw new InvalidArgumentException("O valor unitário deve ser maior que zero");
+        }
+    }
+
+    public function SetDataVencimento($parametroDataVencimento): void
+    {
+        $this->dataVencimento = $parametroDataVencimento;
+    }
+
+    public function GetNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function GetValorUnitario()
+    {
+        return $this->valorUnitario;
+    }
+
+    public function GetDataVencimento()
+    {
+        return date_format($this->dataVencimento, "d/m/Y");
+    }
+
+    public function VerificarSeEstaVencido()
+    {
+        $dataHoje = new DateTime();
+
+        if ($this->dataVencimento > $dataHoje) {
+            echo "Válido";
+        } else {
+            echo "Vencido";
+        }
+    }
+}
+
+?>
