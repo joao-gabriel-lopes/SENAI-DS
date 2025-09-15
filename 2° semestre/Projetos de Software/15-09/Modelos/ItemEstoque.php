0485792b7f9ce9fbf6d0c97535da39d7f48ae7ef
João <jgablopes2008@gmail.com>
2025-09-15 16:02:38 -0300

    Classes remédios

---
+++ 2° semestre/Projetos de Software/15-09/Modelos/ItemEstoque.php
@@ -0,0 +1,96 @@
+<?php
+
+abstract class ItemEstoque
+{
+    protected $id;
+    protected $nome;
+    protected $descricao;
+    protected $quantidade;
+    protected $fornecedor;
+    protected $dataFabricacao;
+    protected $dataValidade;
+
+    public function __construct($id, $nome, $descricao, $quantidade, $fornecedor, $dataFabricacao, $dataValidade)
+    {
+        $this->SetId($id);
+        $this->SetNome($nome);
+        $this->SetDescricao($descricao);
+        $this->SetQuantidade($quantidade);
+        $this->SetFornecedor($fornecedor);
+        $this->SetDataFabricacao($dataFabricacao);
+        $this->SetDataValidade($dataValidade);
+    }
+
+    public function SetId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function SetNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function SetDescricao($descricao)
+    {
+        $this->descricao = $descricao;
+    }
+
+    public function SetQuantidade($quantidade)
+    {
+        $this->quantidade = $quantidade;
+    }
+
+    public function SetFornecedor($fornecedor)
+    {
+        $this->fornecedor = $fornecedor;
+    }
+
+    public function SetDataFabricacao($dataFabricacao)
+    {
+        $this->dataFabricacao = $dataFabricacao;
+    }
+
+    public function SetDataValidade($dataValidade)
+    {
+        $this->dataValidade = $dataValidade;
+    }
+
+    public function GetId()
+    {
+        return $this->id;
+    }
+
+    public function GetNome()
+    {
+        return $this->nome;
+    }
+
+    public function GetDescricao()
+    {
+        return $this->descricao;
+    }
+
+    public function GetQuantidade()
+    {
+        return $this->quantidade;
+    }
+
+    public function GetFornecedor()
+    {
+        return $this->fornecedor;
+    }
+
+    public function GetDataFabricacao()
+    {
+        return $this->dataFabricacao;
+    }
+
+    public function GetDataValidade()
+    {
+        return $this->dataValidade;
+    }
+
+}
+
+?>
---
+++ 2° semestre/Projetos de Software/15-09/Modelos/Remedio.php
@@ -0,0 +1,72 @@
+<?php
+require_once 'ItemEstoque.php';
+
+class Remedio extends ItemEstoque
+{
+    private $principioAtivo;
+    private $formaDeAplicacao;
+    private $categoria;
+    private $classificacao;
+    private $exigeControleRigoroso;
+
+    public function __construct($id, $nome, $descricao, $quantidade, $fornecedor, $dataFabricacao, $dataValidade, $principioAtivo, $formaDeAplicacao, $categoria, $classificacao, $exigeControleRigoroso)
+    {
+        Parent::__construct($id, $nome, $descricao, $quantidade, $fornecedor, $dataFabricacao, $dataValidade);
+        $this->SetPrincipioAtivo($principioAtivo);
+        $this->SetFormaDeAplicacao($formaDeAplicacao);
+        $this->setCategoria($categoria);
+        $this->setClassificacao($classificacao);
+        $this->SetExigeControleRigoroso($exigeControleRigoroso);
+    }
+
+    public function SetPrincipioAtivo($principioAtivo)
+    {
+        $this->principioAtivo = $principioAtivo;
+    }
+    public function SetFormaDeAplicacao($formaDeAplicacao)
+    {
+        $this->formaDeAplicacao = $formaDeAplicacao;
+    }
+
+    public function SetCategoria($categoria)
+    {
+        $this->categoria = $categoria;
+    }
+
+    public function SetClassificacao($classificacao)
+    {
+        $this->classificacao = $classificacao;
+    }
+
+    public function SetExigeControleRigoroso($exigeControleRigoroso)
+    {
+        $this->exigeControleRigoroso = $exigeControleRigoroso;
+    }
+
+    public function GetPrincipioAtivo()
+    {
+        return $this->principioAtivo;
+    }
+
+    public function GetFormaDeAplicacao()
+    {
+        return $this->formaDeAplicacao;
+    }
+
+    public function GetCategoria()
+    {
+        return $this->categoria;
+    }
+
+    public function GetClassificacao()
+    {
+        return $this->classificacao;
+    }
+
+    public function GetExigeControleRigoroso()
+    {
+        return $this->exigeControleRigoroso;
+    }
+}
+
+?>
