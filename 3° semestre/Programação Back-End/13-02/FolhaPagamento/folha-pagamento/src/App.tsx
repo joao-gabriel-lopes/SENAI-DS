50bf318e4e53ea647962e2a931e703b2b34088b8
João <jgablopes2008@gmail.com>
2026-02-13 14:30:11 -0300

    Tela da API folha de pagamento

---
+++ 3° semestre/Programação Back-End/13-02/FolhaPagamento/folha-pagamento/src/App.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react'
+import Card from '@mui/material/Card';
+import { CardContent, CardMedia } from '@mui/material';
+import './App.css'
+
+interface IFuncionario {
+  nome: String,
+  urlFoto: String,
+  tipo: String,
+  pagamento: Number
+}
+
+function App() {
+
+  const [lista, setLista] = useState<IFuncionario[]>([]);
+
+  async function CarregarDados() {
+    const resposta = await fetch('https://localhost:7028/funcionarios');
+    const dados = await resposta.json();
+    setLista(dados);
+  }
+
+  useEffect(() => { CarregarDados() }, []);
+
+  function FuncionarioCard(funcionario: IFuncionario) {
+    return(      
+      <Card className='card'>
+        <CardContent className='conteudo-card'>
+          <CardMedia
+            className='img-card'
+            component="img"
+            height="200"
+            image={funcionario?.urlFoto.toString()}
+            alt="Perfil"
+          />
+          <p className='titulo-card'>{funcionario?.nome}</p>
+          <p className='texto-card'>Pagamento: {funcionario?.pagamento?.toString()}</p>
+          <p className='texto-card'>Tipo: {funcionario?.tipo}</p>
+        </CardContent>
+      </Card>
+    )
+  }
+
+  return (
+    <>
+      <h1 className='titulo'>Funcionários</h1>
+      <div className='container'>
+        {lista.map((funcionario) => (
+          FuncionarioCard(funcionario)
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default App
