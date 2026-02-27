d29c63cadff03dbb809cc1ab03ee72f7ba468261
João <jgablopes2008@gmail.com>
2026-02-27 14:58:11 -0300

    Modificadores de acesso

---
+++ 3° semestre/Programação Back-End/27-02/ModificadoresAcesso/BibliotecaClasses1/ClassePublica.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualBasic;
+
+namespace BibliotecaClasses1;
+
+public class ClassePublica
+{
+    public string? Texto { get; set; } = "Essa é uma classe pública";
+    internal string? Nome { get; set; }
+    private DateOnly _dataNascimento { get; set; }
+    protected int Idade { get; set; }
+
+    public void setDataNascimento(DateOnly data)
+    {
+        DateOnly hoje = DateOnly.FromDateTime(DateTime.Now);
+
+        if (data > hoje)
+        {
+            throw new ArgumentException("A data não pode ser maior que a data de hoje");
+        }
+
+        _dataNascimento = data;
+    }
+}
---
+++ 3° semestre/Programação Back-End/27-02/ModificadoresAcesso/Console1/Program.cs
@@ -0,0 +1,11 @@
+﻿using BibliotecaClasses1;
+
+ClassePublica publica = new();
+
+publica.setDataNascimento(new DateOnly(2000, 2, 27));
+
+//publica.setDataNascimento(new DateOnly(2100, 2, 27)); Causa um erro por não passar no set
+
+//Console.WriteLine(publica.Nome); Causa um erro pois o atributo é internal
+
+Console.WriteLine("Deu certo");
