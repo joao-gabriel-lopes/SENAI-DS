ad339593b30f46f7117698d81db0aeb557351bd5
João <jgablopes2008@gmail.com>
2026-04-17 16:42:32 -0300

    Continuação da api controle de estoque

---
+++ 3° semestre/Programação Back-End/17-04/ControleEstoque/ApiControleEstoque/Contracts/PostProdutoRequest .cs
@@ -0,0 +1,10 @@
+﻿namespace ApiControleEstoque.Contracts;
+
+public record PostProdutoRequest
+{
+    public required Guid CategoriaProdutoId { get; set; }
+    public required Guid UnidadeMedidaId { get; set; }
+    public required string Nome { get; set; }
+    public string? Descricao { get; set; }
+    public decimal? QuantidadeAtual { get; set; }
+}
---
+++ 3° semestre/Programação Back-End/17-04/ControleEstoque/ApiControleEstoque/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ApiControleEstoque.Data;
+using ApiControleEstoque.Contracts;
+using ProdutoDomain;
+using Mapster;
+
+namespace ApiControleEstoque
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<ApiControleEstoqueContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("ApiControleEstoqueContext") ?? throw new InvalidOperationException("Connection string 'ApiControleEstoqueContext' not found.")));
+
+            // Add services to the container.
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            TypeAdapterConfig<PatchProdutoRequest, Produto>.NewConfig().IgnoreNullValues(true);
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
