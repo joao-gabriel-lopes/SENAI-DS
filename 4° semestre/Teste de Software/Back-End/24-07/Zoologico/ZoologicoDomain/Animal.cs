284d3165b457ab8ca23f5de6f3d180c21b51dc5a
João <jgablopes2008@gmail.com>
2026-07-24 11:02:56 -0300

    Testes zoologico

---
+++ 4° semestre/Teste de Software/Back-End/24-07/Zoologico/ZoologicoDomain/Animal.cs
@@ -0,0 +1,52 @@
+﻿namespace ZoologicoDomain;
+
+public class Animal
+{
+    public String Nome { get; set; }
+    public String Especie { get; set; }
+    public String Genero { get; set; }
+    public String Sexo { get; set; }
+    public int Idade { get; set; }
+    public DateOnly DataEntrada { get; set; }
+
+    public Animal(string nome, string especie, string genero, string sexo, int idade, DateOnly dataEntrada)
+    {
+        if (string.IsNullOrWhiteSpace(nome))
+        {
+            throw new ArgumentNullException("O nome do animal não pode ser nulo ou vazio");
+        }
+
+        if (string.IsNullOrWhiteSpace(especie))
+        {
+            throw new ArgumentNullException("A espécie do animal não pode ser nula nem vazia");
+        }
+
+        if (string.IsNullOrWhiteSpace(genero))
+        {
+            throw new ArgumentNullException("O gênero do animal não pode ser nulo ou vazio");
+        }
+
+        if (sexo.ToUpper() != "M" && sexo.ToUpper() != "F")
+        {
+            throw new ArgumentException("O sexo do animal deve ser M ou F");
+        }
+
+        if (idade < 0)
+        {
+            throw new ArgumentException("A idade do animal não pode ser menor que zero");
+        }
+
+        DateOnly dataAtual = DateOnly.FromDateTime(DateTime.Now);
+        if (dataEntrada > dataAtual)
+        {
+            throw new ArgumentException("A data de entrada não pode ser maior que a data de hoje");
+        }
+
+        Nome = nome;
+        Especie = especie;
+        Genero = genero;
+        Sexo = sexo;
+        Idade = idade;
+        DataEntrada = dataEntrada;
+    }
+}
---
+++ 4° semestre/Teste de Software/Back-End/24-07/Zoologico/ZoologicoTeste/AnimalTest.cs
@@ -0,0 +1,142 @@
+﻿using ZoologicoDomain;
+
+namespace ZoologicoTeste;
+
+[TestClass]
+public sealed class AnimalTest
+{
+    [TestMethod]
+    public void VerificarNomeNulo()
+    {
+        Action instanciar = () => new Animal(
+            null!,
+            "Catus",
+            "Felis",
+            "F",
+            10,
+            new DateOnly(2025, 10, 5)
+        );
+
+        Assert.Throws<ArgumentNullException>(instanciar);
+    }
+
+    [TestMethod]
+    public void VerificarNomeVazio()
+    {
+        Action instanciar = () => new Animal(
+            "",
+            "Catus",
+            "Felis",
+            "F",
+            10,
+            new DateOnly(2025, 10, 5)
+        );
+
+        Assert.Throws<ArgumentNullException>(instanciar);
+    }
+
+    [TestMethod]
+    public void VerificarEspecieNula()
+    {
+        Action instanciar = () => new Animal(
+            "Gato",
+            null!,
+            "Felis",
+            "F",
+            10,
+            new DateOnly(2025, 10, 5)
+        );
+
+        Assert.Throws<ArgumentNullException>(instanciar);
+    }
+
+    [TestMethod]
+    public void VerificarEspecieVazia()
+    {
+        Action instanciar = () => new Animal(
+            "Gato",
+            "",
+            "Felis",
+            "F",
+            10,
+            new DateOnly(2025, 10, 5)
+        );
+
+        Assert.Throws<ArgumentNullException>(instanciar);
+    }
+
+    [TestMethod]
+    public void VerificarGeneroNulo()
+    {
+        Action instanciar = () => new Animal(
+            "Gato",
+            "Catus",
+            null!,
+            "F",
+            10,
+            new DateOnly(2025, 10, 5)
+        );
+
+        Assert.Throws<ArgumentNullException>(instanciar);
+    }
+
+    [TestMethod]
+    public void VerificarGeneroVazio()
+    {
+        Action instanciar = () => new Animal(
+            "Gato",
+            "Catus",
+            "",
+            "F",
+            10,
+            new DateOnly(2025, 10, 5)
+        );
+
+        Assert.Throws<ArgumentNullException>(instanciar);
+    }
+
+    [TestMethod]
+    public void VerificarSexoErrado()
+    {
+        Action instanciar = () => new Animal(
+            "Gato",
+            "Catus",
+            "Felis",
+            "hj",
+            10,
+            new DateOnly(2025, 10, 5)
+        );
+
+        Assert.Throws<ArgumentException>(instanciar);
+    }
+
+    [TestMethod]
+    public void VerificarIdadeNegativa()
+    {
+        Action instanciar = () => new Animal(
+            "Gato",
+            "Catus",
+            "Felis",
+            "F",
+            -5,
+            new DateOnly(2025, 10, 5)
+        );
+
+        Assert.Throws<ArgumentException>(instanciar);
+    }
+
+    [TestMethod]
+    public void VerificarDataMaiorQueHoje()
+    {
+        Action instanciar = () => new Animal(
+            "Gato",
+            "Catus",
+            "Felis",
+            "F",
+            10,
+            new DateOnly(2028, 10, 5)
+        );
+
+        Assert.Throws<ArgumentException>(instanciar);
+    }
+}
