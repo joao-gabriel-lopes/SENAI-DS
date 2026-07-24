ae03682d28b8af56c7cfc305a24c0c7ba053eb72
João <jgablopes2008@gmail.com>
2026-07-24 10:19:04 -0300

    Testes básicos

---
+++ 4° semestre/Teste de Software/Back-End/24-07/Formula1/Formula1Domain/Equipe.cs
@@ -0,0 +1,32 @@
+﻿namespace Formula1Domain;
+
+public class Equipe
+{
+    public Guid Id { get; set; }
+    public string Nome { get; set; }
+    public DateOnly DataFundacao { get; set; }
+    public double ValorEmCaixa { get; set; }
+
+    public Equipe(Guid id, string nome, DateOnly dataFundacao, double valorEmCaixa)
+    {
+        if (string.IsNullOrEmpty(nome))
+        {
+            throw new ArgumentNullException("A equipe deve conter um nome não nulo");
+        }
+
+        if (nome.Length < 3)
+        {
+            throw new ArgumentException("O nome da equipe deve ter pelo menos 3 caracteres");
+        }
+
+        if(valorEmCaixa < 0)
+        {
+            throw new ArgumentException("O valor em caixa não pode ser negativo");
+        }
+
+        Id = id;
+        Nome = nome;
+        DataFundacao = dataFundacao;
+        ValorEmCaixa = valorEmCaixa;
+    }
+}
---
+++ 4° semestre/Teste de Software/Back-End/24-07/Formula1/Formula1Teste/EquipeTest.cs
@@ -0,0 +1,125 @@
+﻿using System.Runtime.CompilerServices;
+using Formula1Domain;
+
+namespace Formula1Teste;
+
+[TestClass]
+public sealed class EquipeTest
+{
+    [TestMethod]
+    public void VerificarNomeNulo()
+    {
+        Action instanciar = () => new Equipe(
+            Guid.NewGuid(),
+            null!,
+            new DateOnly(2025, 5, 10),
+            50
+        );
+
+        Assert.Throws<ArgumentNullException>(instanciar);
+    }
+
+    [TestMethod]
+    public void VerificarNomeVazio()
+    {
+        Action instanciar = () => new Equipe(
+            Guid.NewGuid(),
+            "",
+            new DateOnly(2025, 5, 10),
+            50
+        );
+
+        Assert.Throws<ArgumentNullException>(instanciar);
+    }
+
+    [TestMethod]
+    public void VerificarNomeUmCaractere()
+    {
+        Action instanciar = () => new Equipe(
+            Guid.NewGuid(),
+            "a",
+            new DateOnly(2025, 5, 10),
+            50
+        );
+
+        Assert.Throws<ArgumentException>(instanciar);
+    }
+
+    [TestMethod]
+    public void VerificarNomeDoisCaracteres()
+    {
+        Action instanciar = () => new Equipe(
+            Guid.NewGuid(),
+            "ab",
+            new DateOnly(2025, 5, 10),
+            50
+        );
+
+        Assert.Throws<ArgumentException>(instanciar);
+    }
+
+    [TestMethod]
+    public void VerificarNomeTresCaracteres()
+    {
+        Action instanciar = () => new Equipe(
+            Guid.NewGuid(),
+            "abc",
+            new DateOnly(2025, 5, 10),
+            50
+        );
+
+        instanciar();
+    }
+
+    [TestMethod]
+    public void VerificarNomeLongo()
+    {
+        Action instanciar = () => new Equipe(
+            Guid.NewGuid(),
+            "Ferrari",
+            new DateOnly(2025, 5, 10),
+            50
+        );
+
+        instanciar();
+    }
+
+    [TestMethod]
+    public void VerificarValorCaixaNegativo()
+    {
+        Action instanciar = () => new Equipe(
+            Guid.NewGuid(),
+            "Ferrari",
+            new DateOnly(2025, 5, 10),
+            -150
+        );
+
+        Assert.Throws<ArgumentException>(instanciar);
+    }
+
+    [TestMethod]
+    public void VerificarValorCaixaZero()
+    {
+        Action instanciar = () => new Equipe(
+            Guid.NewGuid(),
+            "Ferrari",
+            new DateOnly(2025, 5, 10),
+            0
+        );
+
+        instanciar();
+    }
+
+    [TestMethod]
+    public void VerificarValorCaixaPositivo()
+    {
+        Action instanciar = () => new Equipe(
+            Guid.NewGuid(),
+            "Ferrari",
+            new DateOnly(2025, 5, 10),
+            50
+        );
+
+        instanciar();
+    }
+}
